38a6b6e0228d9b7b15fc2bf83ebbe05629d3ac40
ilahyani <lahyani799@gmail.com>
2024-08-29 20:57:13 +0100

    EC2 iam role to access ECR and pull service image

--- auth-service/infrastructure/iam.tf
+++ auth-service/infrastructure/iam.tf
@@ -1,3 +1,51 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role_for_ecr"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ecr_access_policy"
+  description = "Policy to allow EC2 access to ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetAuthorizationToken"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  policy_arn = aws_iam_policy.ecr_policy.arn
+  role       = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 # resource "aws_iam_role" "lambda_iam_role" {
 #   name = "lambda_iam_role"
 #   assume_role_policy = jsonencode({
