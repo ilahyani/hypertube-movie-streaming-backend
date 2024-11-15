91ac432aa6216753b7931f4c5271bf2547306d39
Smail Lahyani <ilahyani@student.1337.ma>
2024-11-15 19:08:20 +0100

    [FEAT] added an nginx api gateway

--- auth-service/infrastructure/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = "repo"
-  image_tag_mutability = "MUTABLE"
-}
--- auth-service/infrastructure/s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "hyper_bucket" {
-  bucket = "hyper-hyper-bucket"
-}
