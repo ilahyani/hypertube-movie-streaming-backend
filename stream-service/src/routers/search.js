51df4563b8e1f4c9561c2b71f11a0e42ca9e6f45
ilahyani <lahyani799@gmail.com>
2025-01-23 21:54:13 +0100

    search router movie details endpoints

--- stream-service/src/routers/search.js
+++ stream-service/src/routers/search.js
@@ -1,21 +1,69 @@
 const router = require("express").Router({mergeParams: true});
 
 router.get('/', async (req, res) => {
-    const query = req.query.q
-    if (!query || query === "") {
-        return res.status(200).json({})
+    let { query, page, pageSize, sort } = req.query
+    if (isNaN(page) || isNaN(pageSize)) {
+        return res.status(400).json({error: 'Invalid page or pageSize parameters'})
     }
-    const url = `https://yts.mx/api/v2/list_movies.json?query_term=${query}`
+    if (!sort) {
+        sort = 'title'
+    } else if (!['title', 'year', 'rating'].includes(sort)) {
+        return res.status(400).json({error: 'Invalid sort parameter'})
+    }
+    if (!query) {
+        query = ""
+        sort = "rating"
+    }
+    const url = `https://yts.mx/api/v2/list_movies.json?sort_by=${sort}&query_term=${query}&limit=${pageSize}&page=${page}`
+    console.log(url)
     try {
         const response = await fetch(url)
         if (!response.ok) {
-            return res.status(500).json({ error: 'Internal Server Error'})
+            return res.status(500).json({ error: 'Failed to fetch data'})
         }
-        const data = await response.json()
-        return res.status(200).json({ data: data.data })
+        const { data } = await response.json()
+        data.movies = data.movies.map((movie) => {
+            // query movies table, if record exists, watched = true else watched = false
+            return {
+                id: movie.id,
+                title: movie.title_english,
+                year: movie.year,
+                rating: movie.rating,
+                thumbnail: movie.large_cover_image,
+                watched: false
+            }
+        })
+        return res.status(200).json({ data })
     }
     catch (error) {
         console.error(error)
+        return res.status(500).json({ error: 'Internal Server Error'})
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const movie_id = req.params.id
+    if (!movie_id) {
+        res.status(400).json({ error: 'Missing movie id' })
+    }
+    const url = `https://yts.mx/api/v2/movie_details.json?movie_id=${movie_id}`
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            return res.status(500).json({ error: 'Failed to fetch data'})
+        }
+        const { data } = await response.json()
+        return res.status(200).json({
+            title: data.movie.title_english,
+            summary: data.movie.description_full,
+            year: data.movie.year,
+            rating: data.movie.rating,
+            genre: data.movie.genres,
+            thumbnail: data.movie.large_cover_image,
+            torrents: data.movie.torrents, 
+            // subtitles
+        })
+    } catch (error) {
         return res.status(500).json({ error: 'Internal Server Error'})
     }
 })
