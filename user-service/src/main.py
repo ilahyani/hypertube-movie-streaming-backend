f3a6a6d0a6d781884a4618ad2f46147d123d6116
ilahyani <lahyani799@gmail.com>
2025-02-18 19:06:49 +0100

    remove authorization middleware

--- user-service/src/main.py
+++ user-service/src/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, Request
 from .api import api as user_api
-# from .middleware.jwt_middleware import jwt_middleware
 
 app = FastAPI()
 
 app.include_router(user_api.router, prefix='/api/user')
-
-# app.middleware('http')(jwt_middleware)
 
 @app.get('/api/user')
 def root(request: Request):
