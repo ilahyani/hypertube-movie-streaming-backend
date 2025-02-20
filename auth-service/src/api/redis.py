624d83c5b21068245f6b3fad219ca3635714fc2e
ilahyani <lahyani799@gmail.com>
2025-02-20 23:49:35 +0100

    [FEAT] added gitlab oauth strategy

--- auth-service/src/api/redis.py
+++ auth-service/src/api/redis.py
@@ -1,9 +1,9 @@
 import os
 import redis.asyncio as redis
 
-redis_client = redis.Redis("redis")
+redis_client = redis.Redis(host="redis", decode_responses=True)
 
-async def add_key_value_redis(key, value):
+async def add_key_value_redis(key, value, expire=None):
     await redis_client.set(key, value)
     if expire:
         await redis_client.expire(key, expire)
