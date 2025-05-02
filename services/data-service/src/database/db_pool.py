85bf2320faec307aab38f885be3f4be9d0c270b9
ilahyani <lahyani799@gmail.com>
2025-05-03 00:16:17 +0100

    FIX: raise conneciton pool max

--- services/data-service/src/database/db_pool.py
+++ services/data-service/src/database/db_pool.py
@@ -15,9 +15,12 @@ class DatabasePool:
                     f"user={os.getenv('POSTGRES_USER')} "
                     f"password={os.getenv('POSTGRES_PASSWORD')} "
                     f"host={os.getenv('POSTGRES_HOST')} "
-                    f"port={os.getenv('POSTGRES_PORT')}"
+                    f"port={os.getenv('POSTGRES_PORT')}",
+                min_size=10,
+                max_size=100,
+                timeout=10
             )
         return cls._instance
 
     def get_pool(self):
-        return self.pool 
+        return self.pool
